1c90472e53dd4737ae0e3ecc13e3f59601da56d1
mermis <mehmet.ermis@esifrance.net>
2024-07-09 10:43:27 +0200

    test_search_flow added

--- test_home.py
+++ test_home.py
@@ -8,3 +8,12 @@ class TestHomePage(BaseCase):
         self.assert_url_contains("sdetunicorns")
 
         self.assert_title_contains("SDET Unicorns")
+
+    def test_search_flow(self):
+        self.open("https://practice-react.sdetunicorns.com/")
+
+        self.click('button[class="search-active"]')
+
+        self.type('input[placeholder="Search"]', 'Lenovo\n')
+
+        self.assert_text('Showing Results for Lenovo', 'h3[class="text-center py-3"]')
